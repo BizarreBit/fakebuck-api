cbf05e69213799e21331e0a1c2c73bbae2cc0ede
BizarreBit <prawarit888@hotmail.com>
2022-07-23 00:16:40 +0700

    feat (controllers and routes) add getMe (with Friend)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,36 @@
-const createError = require("../utilities/createError");
-const { User } = require("../models");
+const FriendService = require("../services/friendService");
 
 exports.getMe = async (req, res, next) => {
   try {
     // const { password, ...user } = req.user.dataValues //Not recommended
-    // const user = {
-    //   id: req.user.id,
-    //   firstName: req.user.firstName,
-    //   lastName: req.user.lastName,
-    //   email: req.user.email,
-    //   phoneNumber: req.user.phoneNumber,
-    //   profileImage: req.user.profileImage,
-    //   coverImage: req.user.coverImage,
-    //   friends: [],
-    // };
-    // res.status(200).json({ user });
-    res.status(200).json({ user: req.user });
+
+    const friends = await FriendService.findAcceptedFriend(req.user.id);
+
+    // req.user.friends = friends //doesn't work !! req.user is not a plain object (getter setter)
+    // res.status(200).json({ user: req.user });
+
+    // res.status(200).json({ user: req.user, friends });  //not conform with API doc
+
+    // res.status(200).json({
+    //   user: {
+    //     // hard code?
+    //     id: req.user.id,
+    //     firstName: req.user.firstName,
+    //     lastName: req.user.lastName,
+    //     email: req.user.email,
+    //     phoneNumber: req.user.phoneNumber,
+    //     profileImage: req.user.profileImage,
+    //     coverImage: req.user.coverImage,
+    //     createdAt: req.user.createdAt,
+    //     updatedAt: req.user.updatedAt
+    //     friends,
+    //   },
+    // });
+
+    // stringify and parse?
+    res
+      .status(200)
+      .json({ user: { ...JSON.parse(JSON.stringify(req.user)), friends } });
   } catch (err) {
     next(err);
   }
