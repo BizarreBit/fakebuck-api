4bb5c2f9f34d7e23138648a1579ca74ddb44597e
BizarreBit <prawarit888@hotmail.com>
2022-08-06 15:35:19 +0700

    feat (controllers and routes) add createComment editeComment deleteComment

---
+++ controllers/commentController.js
@@ -0,0 +1,101 @@
+const createError = require("../utilities/createError");
+const { Comment, Post } = require("../models");
+
+exports.createComment = async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+    const { message } = req.body;
+
+    // const post = await Post.findOne({ where: { id: postId } });
+    // if (!post) {
+    //   createError("the post is not exist or deleted", 400);
+    // }
+
+    const comment = await Comment.create({
+      message,
+      postId,
+      userId: req.user.id,
+    });
+
+    res.status(201).json({ comment });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.editComment = async (req, res, next) => {
+  try {
+    const { id, postId } = req.params;
+    const { message } = req.body;
+
+    // const post = await Post.findOne({ where: { id: postId } });
+    // if (!post) {
+    //   createError("the post is not exist or deleted", 400);
+    // }
+
+    // const comment = await Comment.findOne({
+    //   where: {
+    //     id,
+    //     postId,
+    //     userId: req.user.id,
+    //   },
+    // });
+
+    // if (!comment) {
+    //   createError(
+    //     "you don't have permission, or the comment is not exist or deleted",
+    //     400
+    //   );
+    // }
+    const comment = await Comment.findOne({ where: { id, postId } });
+    if (!comment) {
+      createError("comment not found", 400);
+    }
+    if (comment.userId !== req.user.id) {
+      createError("you have no permission", 400);
+    }
+
+    comment.message = message;
+    await comment.save();
+
+    res.status(200).json({ comment });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteComment = async (req, res, next) => {
+  try {
+    const { id, postId } = req.params;
+
+    // const comment = await Comment.findOne({
+    //   where: {
+    //     id,
+    //     postId,
+    //     userId: req.user.id,
+    //   },
+    // });
+
+    // if (!comment) {
+    //   createError(
+    //     "you don't have permission, or the comment comis not exist or deleted",
+    //     400
+    //   );
+    // }
+
+    const comment = await Comment.findOne({ where: { id, postId } });
+
+    if (!comment) {
+      createError("comment not found", 400);
+    }
+    if (comment.userId !== req.user.id) {
+      createError("you have no permission", 400);
+    }
+
+    await comment.destroy();
+
+    res.status(204).json();
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -2,11 +2,15 @@ const router = require("express").Router();
 
 const upload = require("../middlewares/upload");
 const postController = require("../controllers/postController");
+const commentController = require("../controllers/commentController")
 
 router.post("/", upload.single("image"), postController.createPost);
 router.patch("/:id", upload.single("image"), postController.editPost);
 router.delete("/:id", postController.deletePost);
 router.post("/:id/like", postController.likePost);
 router.delete("/:id/like", postController.unlikePost);
+router.post("/:postId/comments", commentController.createComment);
+router.patch("/:postId/comments/:id", commentController.editComment);
+router.delete("/:postId/comments/:id", commentController.deleteComment);
 
 module.exports = router;
